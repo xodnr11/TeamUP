44c9473fd5b593d721d46ab5a3021325aa48622f
taeuk <971021q@naver.com>
2022-06-21 14:44:51 +0900

    Initial commit

---
+++ src/main/java/com/example/TeamUP/TeamUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeamUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamUpApplication.class, args);
+	}
+
+}
